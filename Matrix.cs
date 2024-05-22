d50c88a2d2835f415b45d9720e7a0b41c815ea93
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-05-23 02:33:12 +0900

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -1,1 +1,71 @@
-C#
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'minTime' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. 2D_INTEGER_ARRAY roads
+     *  2. INTEGER_ARRAY machines
+     */
+
+    public static int Dfs(int current, int parent, List<List<Tuple<int, int>>> graph, HashSet<int> machines, ref int result) {
+    }
+
+    public static int minTime(List<List<int>> roads, List<int> machines)
+    {
+      
+    }
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> roads = new List<List<int>>();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            roads.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(roadsTemp => Convert.ToInt32(roadsTemp)).ToList());
+        }
+
+        List<int> machines = new List<int>();
+
+        for (int i = 0; i < k; i++)
+        {
+            int machinesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            machines.Add(machinesItem);
+        }
+
+        int result = Result.minTime(roads, machines);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
