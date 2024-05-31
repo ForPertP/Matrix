fbc4d34364ea492f7e9cce9a27a26b0a25fcfd57
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-06-01 04:57:03 +0900

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -25,57 +25,75 @@ class Result
      */
 
     public static int Dfs(int current, int parent, List<List<Tuple<int, int>>> graph, HashSet<int> machines, ref int result)
-        int dfs(int current, int parent, const Vector<Vector<Pair<int, int>>>& graph, const unordered_set<int>& machines, int& result)
-        int dfs(int current, int parent, const vector<vector<pair<int, int>>>& graph, const unordered_set<int>& machines, int& result)
-{
+    //int dfs(int current, int parent, const vector<vector<pair<int, int>>>& graph, const unordered_set<int>& machines, int& result)
     {
         List<int> values = new List<int>();
+        //vector<int> values;
 
         foreach (var neighbor in graph[current])
+        //for (const auto& neighbor : graph[current])
         {
             int next = neighbor.Item1;
+            //int next = neighbor.first;
             int weight = neighbor.Item2;
+            //int weight = neighbor.second;
             if (next == parent) continue;
 
             int temp = Dfs(next, current, graph, machines, ref result);
             if (temp > 0)
             {
                 values.Add(Math.Min(temp, weight));
+                //values.push_back(min(temp, weight));
             }
         }
 
         if (!machines.Contains(current))
+        //if (machines.find(current) == machines.end())
         {
             if (!values.Any()) return -1;
+            //if (values.empty()) return -1;
+            
             if (values.Count == 1) return values[0];
+            //if (values.size() == 1) return values[0];
 
             values.Sort();
+            // std::sort(values.begin(), values.end());
 
             for (int i = 0; i < values.Count - 1; i++)
+            // for (size_t i = 0; i < values.size() - 1; i++)
             {
                 result += values[i];
             }
 
             return values.Last();
+            // return values.back();
         }
         else
         {
             foreach (var value in values)
+            // for( const auto& value : values)
             {
                 result += value;
             }
 
             return int.MaxValue;
+            //return INT_MAX
         }
     }
 
     public static int minTime(List<List<int>> roads, List<int> machines)
+    // int minTime(const vector<vector<int>>& roads, const vector<int>& machines)
     {
         int n = roads.Count + 1;
+        //int n = roads.size() + 1;
+        
         int result = 0;
 
         HashSet<int> machinesSet = new HashSet<int>(machines);
+        //unordered_set<int> machinesSet(machines.begin(), machines.end());
+        
         List<List<Tuple<int, int>>> graph = new List<List<Tuple<int, int>>>(new List<Tuple<int, int>>[n]);
+        //vecotr<vector<pair<int, int>>> graph(n);
 
         for (int i = 0; i < n; i++)
         {
@@ -83,12 +101,15 @@ class Result
         }
 
         foreach (var road in roads)
+        // for (const auto& road : roads)
         {
             int a = road[0];
             int b = road[1];
             int d = road[2];
             graph[a].Add(new Tuple<int, int>(b, d));
+            //graph[a].push_back(std::make_pair(b, d));
             graph[b].Add(new Tuple<int, int>(a, d));
+            //graph[b].push_back({a, d});
         }
 
         Dfs(0, -1, graph, machinesSet, ref result);
